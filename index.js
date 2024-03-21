bcffe51b947b23aed5f7a42e6ccbc044c0805be4
Gideon <keterjr1@gmail.com>
2024-03-21 21:24:03 +0300

    manipulate arrays

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat () {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat () {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat () {
+    cats.shift();
+}
+
+function appendCat () {
+    let arr = [...cats]
+    arr.push("Broom")
+    return arr
+}
+
+function prependCat () {
+    let arr = [...cats]
+    arr.unshift("Arnold")
+    return arr
+}
+
+function removeLastCat () {
+    let arr = [...cats]
+    arr.pop()
+    return arr
+}
+
+function removeFirstCat () {
+    let arr = [...cats]
+    arr.shift()
+    return arr
+}
